d4f7aaea86dcb1fa99f2ee8688be134882082dc4
Rodrigo Noll <rodrigo.noll@canoas.ifrs.edu.br>
2016-03-07 01:53:34 -0300

    added error handling and role constraints

---
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/admin/AdminUserController.java
@@ -0,0 +1,20 @@
+package br.edu.ifrs.canoas.lds.ifskills.controller.admin;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@Secured( {"ROLE_ADMIN"} )
+@RequestMapping("/admin/user")
+public class AdminUserController {
+	
+	
+	@RequestMapping("/view")
+	@ResponseBody
+	public String view(Model model){
+		return "Authorized user to acess something!";
+	}	
+}
