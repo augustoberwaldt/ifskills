e29b80aac196653929cf9eca07eac5de25eb7da8
Luciane <Luciane@Luciane-PC>
2016-03-06 22:54:31 -0300

    Criei um item de menu chamdo Profiles, para que eu possa carregar a lista de perfis cadastrados. Também criei dois itens deste menu, o Listar perfis, e o Criar novo perfil, mas este dois não apareceram

---
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/admin/ProfileController.java
@@ -0,0 +1,12 @@
+package br.edu.ifrs.canoas.lds.ifskills.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/adimin/user/")
+public class ProfileController {
+	//list
+	//public  
+
+}
