d5d343f2dddb139166d12b866906fb3dd570f88e
Luciane <lucianes2009@hotmail.com>
2016-03-10 23:27:53 -0300

    Problemas no campo de pesquisa

--- src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/ProfileController.java
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/ProfileController.java
@@ -1,25 +1,11 @@
 package br.edu.ifrs.canoas.lds.ifskills.controller;
 
-import java.io.IOException;
-import java.util.Locale;
-
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.edu.ifrs.canoas.lds.ifskills.domain.Item;
-import br.edu.ifrs.canoas.lds.ifskills.domain.User;
 import br.edu.ifrs.canoas.lds.ifskills.service.UserProfileService;
 
 @Controller
@@ -27,16 +13,14 @@ import br.edu.ifrs.canoas.lds.ifskills.service.UserProfileService;
 public class ProfileController {
 
 	private UserProfileService userProfileService;
-	private MessageSource messageSource;
 
 	@Autowired
-	public ProfileController(UserProfileService service, MessageSource messageSource) {
+	public ProfileController(UserProfileService service) {
 		this.userProfileService = service;
-		this.messageSource = messageSource;
 	}
 
 	@RequestMapping("/search")
-	public String list(Model model){
+	public String list(Model model) {
 		model.addAttribute("users", userProfileService.list());
 		return "/profile/list";
 	}
@@ -48,20 +32,5 @@ public class ProfileController {
 		return "/profile/form";
 	}//tentativa 09/03
 	
-	
-	@RequestMapping(value = "/search", method = RequestMethod.POST)
-	public String save(@Valid User user, BindingResult bindingResult, Model model, RedirectAttributes redirectAttrs, Locale locale) {
-		if(!bindingResult.hasErrors() ){
-			User savedProfile = userProfileService.save(user);
-			model.addAttribute("readonly", true);
-			
-			redirectAttrs.addFlashAttribute("message", messageSource.getMessage("item.saved", null, locale));
-			
-			return "redirect:/profile/list/" + savedProfile.getId()+"?success";		
-		}
-		model.addAttribute("readonly", false);
-		return "/profile/list";
-	}	
-
 
 }
