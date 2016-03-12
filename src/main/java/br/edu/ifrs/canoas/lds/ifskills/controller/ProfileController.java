95f6f44475b31bbaf20ee98ea42732e2914eda8c
Luciane <lucianes2009@hotmail.com>
2016-03-12 01:02:29 -0300

    Tentativa de colocar mensagem, quando o perfil do usuário pesquisado não é encontrado!

--- src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/ProfileController.java
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/ProfileController.java
@@ -1,17 +1,24 @@
 package br.edu.ifrs.canoas.lds.ifskills.controller;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.edu.ifrs.canoas.lds.ifskills.domain.Item;
+import br.edu.ifrs.canoas.lds.ifskills.domain.User;
 import br.edu.ifrs.canoas.lds.ifskills.service.UserProfileService;
 
 @Controller
@@ -41,10 +48,24 @@ public class ProfileController {
 	}//tentativa 09/03
 	
 	
-	@RequestMapping(value = "/searchBy", method = RequestMethod.POST)
+	/*@RequestMapping(value = "/searchBy", method = RequestMethod.POST)
 	public String showUserList(Model model,final HttpServletRequest req) {
 		model.addAttribute("users", userProfileService.list(req.getParameter("criteria")));
 		return "/profile/list";
+	}	*/
+	
+	@RequestMapping(value = "/searchBy", method = RequestMethod.POST)
+	public String showUserList(@Valid User user, Model model,final HttpServletRequest req, BindingResult bindingResult, RedirectAttributes redirectAttrs, Locale locale) {
+		if(!bindingResult.hasErrors()){
+			model.addAttribute("users", userProfileService.list(req.getParameter("criteria")));
+			model.addAttribute("readonly", true);
+			
+			redirectAttrs.addFlashAttribute("message", messageSource.getMessage("profile.findAllByFullNameOrEmailAllIgnoreCase", null, locale));
+			
+			return "redirect:/profile/list/";			
+		}
+		model.addAttribute("readonly", false);
+		return "/profile/list";	
 	}	
 
 
