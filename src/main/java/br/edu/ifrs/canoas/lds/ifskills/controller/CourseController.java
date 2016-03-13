1ea8718346c4105c697ed1837cd767ee796d6e08
FernandoSturzbecher <fernandobaandeira@gmail.com>
2016-03-13 17:47:28 -0300

    Added CRUD methods to Course class and relative CourseController; Made minor changes to "course/list.html" to run CourseController methods;

--- src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/CourseController.java
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/CourseController.java
@@ -1,10 +1,21 @@
 package br.edu.ifrs.canoas.lds.ifskills.controller;
 
+import java.text.MessageFormat;
+import java.util.Locale;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.edu.ifrs.canoas.lds.ifskills.domain.Course;
 import br.edu.ifrs.canoas.lds.ifskills.service.CourseService;
 
 @Controller
@@ -12,6 +23,7 @@ import br.edu.ifrs.canoas.lds.ifskills.service.CourseService;
 public class CourseController {
 
 	private CourseService courseService;
+	private MessageSource messageSource;
 		@Autowired
 		public CourseController(CourseService courseService) {
 			this.courseService = courseService;
@@ -21,53 +33,53 @@ public class CourseController {
 		public String list(Model model) {
 			model.addAttribute("courses", courseService.list());
 			return "/course/list";
-		}/*
-		
-		@RequestMapping("/create")
-		public String create(Model model) {
-			model.addAttribute("class", new Class());
-			model.addAttribute("readonly", false);
-			return "/item/form";
-		}
-
-		@RequestMapping("/view/{id}")
-		public String view(@PathVariable Long id, Model model) {
-			model.addAttribute("item", itemService.get(id));
-			model.addAttribute("readonly", true);
-			return "/item/form";
-		}
-
-		@RequestMapping("/edit/{id}")
-		public String update(@PathVariable Long id, Model model) {
-			model.addAttribute("item", itemService.get(id));
-			model.addAttribute("readonly", false);
-			return "/item/form";
 		}
 		
 		@RequestMapping("/delete/{id}")
 		public String delete(@PathVariable Long id, Model model, RedirectAttributes redirectAttrs, Locale locale) {
-			Item item = itemService.get(id);
-			itemService.delete(id);
+			Course course = courseService.get(id);
+			courseService.delete(id);
 			
 			redirectAttrs.addFlashAttribute("message", 
-					MessageFormat.format(messageSource.getMessage("item.deleted", null, locale), 
-							item.getName()));
+					MessageFormat.format(messageSource.getMessage("course.deleted", null, locale), 
+							course.getName()));
 			
-			return "redirect:/item/list";
+			return "redirect:/course/list";
+		}
+				
+		@RequestMapping("/create")
+		public String create(Model model) {
+			model.addAttribute("class", new Course());
+			model.addAttribute("readonly", false);
+			return "/course/form";
+		}
+
+		@RequestMapping("/view/{id}")
+		public String view(@PathVariable Long id, Model model) {
+			model.addAttribute("course", courseService.get(id));
+			model.addAttribute("readonly", true);
+			return "/course/form";
+		}
+
+		@RequestMapping("/edit/{id}")
+		public String update(@PathVariable Long id, Model model) {
+			model.addAttribute("course", courseService.get(id));
+			model.addAttribute("readonly", false);
+			return "/course/form";
 		}	
 
 		@RequestMapping(value = "/save", method = RequestMethod.POST)
-		public String save(@Valid Item item, BindingResult bindingResult, Model model, RedirectAttributes redirectAttrs, Locale locale) {
+		public String save(@Valid Course course, BindingResult bindingResult, Model model, RedirectAttributes redirectAttrs, Locale locale) {
 			if(!bindingResult.hasErrors() ){
-				Item savedItem = itemService.save(item);
+				Course savedCourse = courseService.save(course);
 				model.addAttribute("readonly", true);
 				
-				redirectAttrs.addFlashAttribute("message", messageSource.getMessage("item.saved", null, locale));
+				redirectAttrs.addFlashAttribute("message", messageSource.getMessage("course.saved", null, locale));
 				
-				return "redirect:/item/view/" + savedItem.getId()+"?success";		
+				return "redirect:/course/view/" + savedCourse.getId()+"?success";		
 			}
 			model.addAttribute("readonly", false);
-			return "/item/form";
-		}*/	
+			return "/course/form";
+		}	
 		
 }
