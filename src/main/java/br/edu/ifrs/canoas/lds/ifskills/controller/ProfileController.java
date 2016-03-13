5ec21ce871cd1ae4f7504f985c1f5d8a88abea4e
Rodrigo Noll <rodrigo.noll@canoas.ifrs.edu.br>
2016-03-13 00:51:10 -0300

    search message feedback

--- src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/ProfileController.java
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/controller/ProfileController.java
@@ -1,23 +1,20 @@
 package br.edu.ifrs.canoas.lds.ifskills.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import br.edu.ifrs.canoas.lds.ifskills.domain.Item;
 import br.edu.ifrs.canoas.lds.ifskills.domain.User;
 import br.edu.ifrs.canoas.lds.ifskills.service.UserProfileService;
 
@@ -55,12 +52,13 @@ public class ProfileController {
 	}	*/
 	
 	@RequestMapping(value = "/searchBy", method = RequestMethod.POST)
-	public String showUserList(@Valid User user, Model model,final HttpServletRequest req, BindingResult bindingResult, RedirectAttributes redirectAttrs, Locale locale) {
-		model.addAttribute("users", userProfileService.list(req.getParameter("criteria")));
-		if(userProfileService.list(req.getParameter("criteria")).isEmpty()){
-			redirectAttrs.addFlashAttribute("message", 
+	public String showUserList(Model model,final HttpServletRequest req, RedirectAttributes redirectAttrs, Locale locale) {
+		List<User> users = userProfileService.list(req.getParameter("criteria"));
+		if(users.isEmpty()){
+			model.addAttribute("message", 
 					messageSource.getMessage("profile.findAllByFullNameOrEmailAllIgnoreCase", null, locale));
 		}
+		model.addAttribute("users", users);
 		return "/profile/list";	
 	}	
 
