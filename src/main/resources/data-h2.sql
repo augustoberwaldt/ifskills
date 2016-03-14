2b26e0cd82321275d044d193a1bd47643ca713a7
felipezoltowski <felipe.zoltowski@gmail.com>
2016-03-13 21:47:56 -0300

    Added "ROLE_TRAINER" role to the data.sql and a role user.

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -3,9 +3,11 @@ INSERT INTO USERS(ID, EMAIL, FULL_NAME, PASSWORD, PICTURE) VALUES (2, 'admin@123
 INSERT INTO USERS(ID, EMAIL, FULL_NAME, PASSWORD, PICTURE) VALUES (3, 'admi3n@123.123', '333 Admin', '$2a$10$cy42kPdxwRGcrx02umaL3O7f7xUKXQtugjXLrM9/DNay.D.WNK3HC', null);
 INSERT INTO USERS(ID, EMAIL, FULL_NAME, PASSWORD, PICTURE) VALUES (4, 'jrobaski@yahoo.com.br', 'José Ricardo Robaski', '$2a$10$g3JVotDZCxNZ8IdmDc5QkugSyyd0FNLNnzL5G1mMUcwn3ammWCM6m', null);
 INSERT INTO USERS(ID, EMAIL, FULL_NAME, PASSWORD, PICTURE) VALUES (5, 'edsoncandio@gmail.com', 'Edson Giovani de Candio', '$2a$10$cy42kPdxwRGcrx02umaL3O7f7xUKXQtugjXLrM9/DNay.D.WNK3HC', null);
+INSERT INTO USERS(ID, EMAIL, FULL_NAME, PASSWORD, PICTURE) VALUES (6, 'trainer@123.123', 'General Trainer', '$2a$10$cy42kPdxwRGcrx02umaL3O7f7xUKXQtugjXLrM9/DNay.D.WNK3HC', null);
 
 insert into roles(id, role) values (1, 'ROLE_USER');
 insert into roles(id, role) values (2, 'ROLE_ADMIN');
+insert into roles(id, role) values (3, 'ROLE_TRAINER');
 
 insert into users_roles (user_id, role_id) values (1, 1);
 insert into users_roles (user_id, role_id) values (2, 2);
