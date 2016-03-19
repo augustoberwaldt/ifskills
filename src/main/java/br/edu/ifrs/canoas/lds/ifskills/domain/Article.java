0f3c1c30d105eb21f03bdd5609d1ddfb28b12f50
felipezoltowski <felipe.zoltowski@gmail.com>
2016-03-19 20:16:12 -0300

    Added empty Article relative classes.

---
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/domain/Article.java
@@ -0,0 +1,5 @@
+package br.edu.ifrs.canoas.lds.ifskills.domain;
+
+public class Article {
+
+}
---
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/service/ArticleService.java
@@ -0,0 +1,5 @@
+package br.edu.ifrs.canoas.lds.ifskills.service;
+
+public class ArticleService {
+
+}
