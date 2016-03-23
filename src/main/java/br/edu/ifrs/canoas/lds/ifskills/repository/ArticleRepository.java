c8e7e3853e327461213c2efd87b3e9a9210835dc
felipezoltowski <felipe.zoltowski@gmail.com>
2016-03-23 13:48:34 -0300

    Added ArticleService and Article repository tests

--- src/main/java/br/edu/ifrs/canoas/lds/ifskills/repository/ArticleRepository.java
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/repository/ArticleRepository.java
@@ -1,10 +1,16 @@
 package br.edu.ifrs.canoas.lds.ifskills.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.edu.ifrs.canoas.lds.ifskills.domain.Article;
 
 public interface ArticleRepository extends CrudRepository<Article, Long>{
+	@Query("SELECT i FROM Item i WHERE i.active=true")
+	List<Article> findChecked();
+
 
 }
 
--- src/test/java/br/edu/ifrs/canoas/lds/ifskills/repository/ArticleRepositoryTest.java
+++ src/test/java/br/edu/ifrs/canoas/lds/ifskills/repository/ArticleRepositoryTest.java
@@ -1,5 +1,26 @@
 package br.edu.ifrs.canoas.lds.ifskills.repository;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import br.edu.ifrs.canoas.lds.ifskills.IFSkillsApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(IFSkillsApplication.class)
 public class ArticleRepositoryTest {
+
+	@Autowired
+	ArticleRepository articleRepository;
+	
+	@Test
+	public void testToFindAllItemsThatAreChecked() {
+		assertThat(articleRepository.findChecked().size(), is(5));
+	}
 
 }
