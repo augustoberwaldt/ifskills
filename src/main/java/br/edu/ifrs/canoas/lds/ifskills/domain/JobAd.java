5357297457c061d555acae757afe890d597d3df7
felipezoltowski <felipe.zoltowski@gmail.com>
2016-04-22 23:20:55 -0300

    Corrected the view and form page.

--- src/main/java/br/edu/ifrs/canoas/lds/ifskills/domain/JobAd.java
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/domain/JobAd.java
@@ -71,6 +71,14 @@ public class JobAd {
 		this.id = id;
 	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
 	public String getDescription() {
 		return description;
 	}
