8dbc3d730c7577cfdf5797ae3dfe54a2941f73c9
jrobaski <jrobaski@yahoo.com.br>
2016-04-24 12:08:01 -0300

    Created findAllByArticle metod in ArticleRankRepository and Getränk method in ArticleRankService.

---
+++ src/test/java/br/edu/ifrs/canoas/lds/ifskills/service/ArticleRankService.java
@@ -0,0 +1,12 @@
+package br.edu.ifrs.canoas.lds.ifskills.service;
+
+public class ArticleRankService {
+	
+	/**
+	 * 24/04/16 - Ricardo - Method to calculate the media rank 
+	 */	
+	public void getRank(){
+		
+	}
+
+}
