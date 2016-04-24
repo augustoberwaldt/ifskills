ce5cc798fba24b9c4fbe7111ac2f87d986bedacb
jrobaski <jrobaski@yahoo.com.br>
2016-04-24 18:36:36 -0300

    Creator the findAllBy Article methods in the repository and the service Getränk

---
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/repository/ArticleRankRepository.java
@@ -0,0 +1,14 @@
+package br.edu.ifrs.canoas.lds.ifskills.repository;
+
+import br.edu.ifrs.canoas.lds.ifskills.domain.Article;
+
+public class ArticleRankRepository {
+
+	/**
+	 * 24/04/16 - Ricardo - Method to find all article to calculate rank
+	 */
+	public void findAllByArticle(Article article) {
+
+	}
+
+}
