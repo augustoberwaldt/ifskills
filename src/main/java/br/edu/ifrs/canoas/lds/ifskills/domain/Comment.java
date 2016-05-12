70c54b1125af8a56546ad0f69f9b1ad9888a9f93
jrobaski <jrobaski@yahoo.com.br>
2016-05-12 08:31:00 -0300

    Remove hidden attribute from comment class

--- src/main/java/br/edu/ifrs/canoas/lds/ifskills/domain/Comment.java
+++ src/main/java/br/edu/ifrs/canoas/lds/ifskills/domain/Comment.java
@@ -20,26 +20,12 @@ public class Comment extends Document {
 	@JoinColumn(name = "document_id", nullable = true)
 	private Document document;
 
-	/**
-	 * Create - 11/05/16 - Ricardo - Attribute to hide offensive comment
-	 */
-	@Column(columnDefinition = "boolean default false")
-	private Boolean hidden;
-
 	public Document getDocument() {
 		return document;
 	}
 
 	public void setDocument(Document document) {
 		this.document = document;
-	}
-
-	public Boolean getHidden() {
-		return hidden;
-	}
-
-	public void setHidden(Boolean hidden) {
-		this.hidden = hidden;
 	}
 
 }
