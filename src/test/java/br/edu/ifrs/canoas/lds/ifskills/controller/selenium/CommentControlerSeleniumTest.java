83f51986b4bbfdbbda4a25c8aed4f33024e7821b
Edward Ramos <kalbzero.ramos@gmail.com>
2016-05-18 09:20:45 -0300

    Add CommentArticle in CommentTestSelenium

--- src/test/java/br/edu/ifrs/canoas/lds/ifskills/controller/selenium/CommentControlerSeleniumTest.java
+++ src/test/java/br/edu/ifrs/canoas/lds/ifskills/controller/selenium/CommentControlerSeleniumTest.java
@@ -44,7 +44,7 @@ public class CommentControlerSeleniumTest extends FluentTest {
 	 */
 	@Test
 	@Ignore
-	public void testNavigation() {
+	public void testNavigationPost() {
 		goTo("http://localhost:8080/post/list");
 		await().atMost(5, TimeUnit.SECONDS)
 				.until(find("#DataTables_Table_0_info", withText("Showing 1 to 6 of 6 entries")));
@@ -86,4 +86,57 @@ public class CommentControlerSeleniumTest extends FluentTest {
 		alert().accept();
 		}
 
+	/**
+	 * @author Edward Ramos
+	 * @date: May/18/2016
+	 * @description: Navigation Test
+	 */
+	@Test
+	@Ignore
+	public void testNavigationArticle() {
+		goTo("http://localhost:8080/article/list");
+		await().atMost(5, TimeUnit.SECONDS)
+				.until(find("#DataTables_Table_0_info", withText("Showing 1 to 6 of 6 entries")));
+		
+		//Search
+		fill("#criteria").with("a");        
+        submit(".btn btn-default");
+        await().atMost(5, TimeUnit.SECONDS);
+		findFirst("A", withText("Get With the Program")).click();
+		await().atMost(5, TimeUnit.SECONDS).until(find(".title", withText("View Article")));
+		
+		//CRUD a comment
+		findFirst("A", withText("Create Comment")).click();
+		fill("#CommentAuthor").with("User test");
+		fill("#CommentBody").with("Creating a comment.");
+		findFirst("A", withText("Save Comment")).click();
+		
+		findFirst("A", withText("Edit Comment")).click();
+		fill("#CommentAuthor").with("User test 2");
+		fill("#CommentBody").with("Creating a edited comment.");
+		findFirst("A", withText("Save Comment")).click();
+		
+		findFirst("A", withText("Edit Comment")).click();
+		findFirst("A", withText("Cancel Comment")).click();
+		alert().accept();
+		findFirst("A", withText("Delete Comment")).click();
+		alert().accept();
+		
+		//Reply
+		findFirst("A", withText("Reply Comment")).click();
+		fill("#CommentAuthor").with("User test");
+		fill("#CommentBody").with("Creating a reply comment.");
+		findFirst("A", withText("Save Comment")).click();
+		
+		findFirst("A", withText("Reply Comment")).click();
+		fill("#CommentAuthor").with("User test 2");
+		fill("#CommentBody").with("Creating a edited reply comment.");
+		findFirst("A", withText("Save Comment")).click();
+		
+		findFirst("A", withText("Reply Comment")).click();
+		findFirst("A", withText("Cancel Comment")).click();
+		alert().accept();
+		findFirst("A", withText("Delete Comment")).click();
+		alert().accept();
+		}
 }
